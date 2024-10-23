675d0af5d4c36ec273cbb351adc67479c92201c6
damoosnz <damien.legrand@bimsolutionscentre.com>
2024-10-23 20:31:45 +1300

    remove testing in index.js

--- index.js
+++ index.js
@@ -4,14 +4,3 @@ export const bimscJs = {
     files: files
 }
 
-// temp during dev
-
-const csvText = await  files.fetchCSV('https://api.knack.com/v1/applications/658014c48daf9200276b71ee/download/asset/67177e7ca1953b02d94aca2c/a030713001971300015dec162.csv')
-// console.log(csvText)
-
-const csvObj = files.parseCSV(csvText)
-console.log(csvObj.length, csvObj[csvObj.length-1])
-
-const csvCleaned = files.cleanCSV(csvObj)
-console.log(csvCleaned[1314])
-console.log(csvCleaned[1315])
